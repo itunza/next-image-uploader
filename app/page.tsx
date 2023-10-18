1faaf674b932db9d6e1cf10b3d6397adf1ef7645
itunza <ewanyoike@itunza.co.ke>
2023-10-18 09:33:15 +0300

    id app

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,152 @@
-import Image from 'next/image'
+'use client'
+
+import React, { useState } from 'react';
+import axios from 'axios';
+import styles from './index.module.css';
+import { BeatLoader } from 'react-spinners';
+
 
 export default function Home() {
+  const [file, setFile] = useState<File | null>(null);
+  const [responseData, setResponseData] = useState<any>(null);
+  const [uploadProgress, setUploadProgress] = useState<number | null>(null);
+  const [isWaitingForResponse, setIsWaitingForResponse] = useState<boolean>(false);
+  const [imageURL, setImageURL] = useState<string | null>(null);
+
+
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setFile(e.target.files[0]);
+      setImageURL(URL.createObjectURL(e.target.files[0])); // Add this line
+
+    }
+  };
+
+  
+  
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    // Set the waiting for response state
+    setIsWaitingForResponse(true);
+
+    try {
+      const res = await axios.post('https://maziwa.spaceai.io/webhook/a13d9e7f-73f0-4370-9244-361b4ea7b4fc', formData, {
+        onUploadProgress: (progressEvent) => {
+          const percentCompleted = progressEvent.total ? Math.round((progressEvent.loaded * 100) / progressEvent.total) : 0;
+          setUploadProgress(percentCompleted);
+        }
+      });
+
+      setResponseData(res.data);
+    } catch (error) {
+      console.error("Error uploading image:", error);
+    } finally {
+      // Reset the waiting for response state
+      setIsWaitingForResponse(false);
+    }
+};
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.container}>
+        
+      <h1>IDENTINTY CHECK ITUNZA-V0.1</h1>
+      <div className={styles.space}></div>
+  
+      <h2>Upload an image to Space AI</h2>
+      <div className={styles.space}></div>
+      
+      <form onSubmit={onSubmit}>
+        <div className={styles.fileinputWrapper}>
+          <label htmlFor="file">File</label>
+          <input type="file" onChange={onFileChange} />
         </div>
-      </div>
+        <div className={styles.buttonWrapper}>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+        
+      {uploadProgress && uploadProgress < 100 && (
+        <div className={styles.progressBar}>
+          <BeatLoader color="#4A90E2" />
+          <p>Uploading... {uploadProgress}%</p>
+        </div>
+      )}
+  
+      {uploadProgress && uploadProgress === 100 && (
+        <div className={styles.progressBar}>
+          <progress value={uploadProgress} max="100"></progress>
+          <p>Uploaded! {uploadProgress}%</p>
+        </div>
+      )}
+  
+      {/* {responseData && (
+        <div>
+          <h2>Response:</h2>
+          <pre>{JSON.stringify(responseData, null, 2)}</pre>
+        </div>
+      )} */}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {isWaitingForResponse && (
+          <div className={styles.progressBar}>
+            <BeatLoader color="#4A90E2" />
+            <p>ID CONFIRMATION IN PROGRESS...</p>
+          </div>
+        )}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {/* {responseData && (
+        <div>
+          <h2>Response:</h2>
+          <pre>{JSON.stringify(responseData, null, 2)}</pre>
+        </div>
+      )} */}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      {responseData && (
+          <div className={styles.responseContainer}>
+            <h2>Response:</h2>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            {/* Display the uploaded image */}
+            {imageURL && <img src={imageURL} alt="Uploaded" className={styles.uploadedImage} />}
+          
+            <div className={styles.details}>
+              <h3>Details:</h3>
+              <ul>
+                <li><strong>Serial Number:</strong> {responseData.data.attributes.serial_number}</li>
+                <li><strong>ID Number:</strong> {responseData.data.attributes.id_number}</li>
+                <li><strong>Full Names:</strong> {responseData.data.attributes.full_names}</li>
+                <li><strong>Date of Birth:</strong> {responseData.data.attributes.date_of_birth}</li>
+                <li><strong>Distric of birth:</strong> {responseData.data.attributes.district_of_birth}</li>
+                <li><strong>Sex:</strong> {responseData.data.attributes.sex}</li>
+                <li><strong>Place of Issue:</strong> {responseData.data.attributes.place_of_issue}</li>
+                <li><strong>Date of Issue:</strong> {responseData.data.attributes.date_of_issue}</li>  
+                {/* ... repeat for other attributes */}
+              </ul>
+            </div>
+          </div>
+        )}
+    </div>
+    
+  );
 }
+
+
+
+// "id": 34,
+// "attributes": {
+//   "serial_number": "231647102",
+//   "id_number": "31030810",
+//   "full_names": "DIETHER ROLF KOEHLER",
+//   "date_of_birth": "07.01.1954",
+//   "sex": "MALE",
+//   "district_of_birth": "GERMANY",
+//   "place_of_issue": "ISLAND",
+//   "date_of_issue": "20.11.2012",
+//   "createdAt": "2023-10-17T19:00:10.032Z",
+//   "updatedAt": "2023-10-17T19:00:10.032Z",
+//   "publishedAt": "2023-10-17T19:00:10.028Z",
+//   "isValidated": false
